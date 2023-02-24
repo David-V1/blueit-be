7ed1a0bffe21ca13952d0d82fd86ae1d9e8745d3
David Villaron <90426008+David-V1@users.noreply.github.com>
2023-02-23 22:25:03 -0600

    Comment GET/POST and persisting.

--- src/main/java/com/blueitapp/blueit/controllers/CommentController.java
+++ src/main/java/com/blueitapp/blueit/controllers/CommentController.java
@@ -1,9 +1,12 @@
 package com.blueitapp.blueit.controllers;
 
+import com.blueitapp.blueit.models.Comment;
 import com.blueitapp.blueit.services.CommentService;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/comments")
@@ -13,5 +16,58 @@ public class CommentController {
     public CommentController(CommentService commentService) {
         this.commentService = commentService;
     }
+
+    //Create
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/post/{postId}/user/{userId}")
+    public void addComment(@RequestBody Comment commentText, @PathVariable Long postId, @PathVariable UUID userId) {
+        try {
+            commentService.addComment(commentText, postId, userId);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.PRECONDITION_FAILED);
+        }
+    }
+
+    //Read
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping()
+    public Iterable<Comment> getAllComments() {
+        return commentService.getAllComments();
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/{id}")
+    public Comment getCommentById(@PathVariable Long id) {
+        try {
+            return commentService.getCommentById(id);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/user/{userId}")
+    public Iterable<Comment> getCommentsByUserId(@PathVariable UUID userId) {
+        try {
+            return commentService.getAllCommentsByUserId(userId);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/post/{postId}")
+    public Iterable<Comment> getCommentsByPostId(@PathVariable Long postId) {
+        try {
+            return commentService.getAllCommentsByPostId(postId);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+    }
+
+
+    //Update
+
+    //Delete
 
 }
